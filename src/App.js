035c75c25dd0dceba77933c37162a515c953038d
unknown <alvinsanudharma@gmail.com>
2021-06-22 12:55:41 +0800

    Splash Screen

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {SplashScreen} from './pages';
+
+const App = () => {
+  return <SplashScreen />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
